60b8bd0151c2a513bb92c8c52d9a190b97b3384f
Marlena Tsang <Marlena345@gmail.com>
2021-10-10 21:30:09 -0400

    commit tree 45094dc407485f3921110c9c5396d5775669c9cf

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Add your code here
+// const formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle",
+// };
+
+// // method: "POST" is missing from the object below
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify(formData)
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   })
+//   .catch(function (error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   })
+  
+
+const userUrl = "http://localhost:3000/users"
+
+const bodyData = {
+  name: "Steve",
+  email: "steve@steve.com"
+}
+
+const objStuff = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify(bodyData)
+}
+
+function submitData() {
+  return fetch (userUrl, objStuff)
+  .then(response => response.json())
+  .then(getId)
+  .catch(errorFunc)
+}
+
+const bodyTag =document.querySelector("body")
+
+function getId(object){
+  console.log (document.body.innerHTML = object.id)
+  }
+
+function errorFunc(error) {
+  document.body.innerHTML = error.message
+}
